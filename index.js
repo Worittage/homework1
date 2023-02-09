6deb958ecccf7480c67790222b82999e9e9abaff
Eugen <worittagebez@gmail.com>
2023-02-09 19:41:32 +0200

    09.02.23

--- index.js
+++ index.js
@@ -1,102 +1,126 @@
-/**1 задание */
-function findSmaller(a, b) {
-  return a < b ? a : b;
-}
+//* 1 задание
+let arr = [1, 5, 4, 10, 0, 3];
 
-let num1 = 8;
-let num2 = 4;
-let smaller = findSmaller(num1, num2);
-alert(smaller);
-
-/**2 задание */
-function evenOrOdd(num) {
-  return num % 2 === 0 ? "Число нечетное" : "если нечетное";
-}
-
-/**3,1 задание  */
-function printSquare(num) {
-  console.log(num * num);
-}
-
-let numbere = 8;
-printSquare(numbere);
-
-/**3.2 задание */
-
-function findSquare(num) {
-  return num * num;
-}
-let number = 8;
-let square = findSquare(number);
-alert(square);
-//////
-
-function cubeNumber() {
-  let num = prompt("Введите номер");
-  if (isNaN(num)) {
-    return "Переданный параметр не является числом";
-  }
-  let result = num * num * num;
-  return `${num} в кубе ровно ${result}`;
-}
-
-for (let i = 0; i <= 10; i++) {
-  alert(cubeNumber(i));
-}
-
-/**4 задание  */
-function askAge() {
-  const age = parseInt(prompt("Сколько вам лет?"));
-  if (age < 0) {
-    console.log("Вы ввели неправильное значение");
-  } else if (age >= 0 && age <= 12) {
-    console.log("Привет, друг!");
-  } else if (age >= 13) {
-    console.log("Добро пожаловать!!");
-  }
-}
-
-/**5 задание */
-function multiplyNumbers(a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return "Одно или оба значения не являются числом";
-  }
-
-  return a * b;
-}
-
-/**6 задание */
-function cubeNumber(n) {
-  if (isNaN(n)) {
-    return "Переданный параметр не является числом";
-  }
-
-  return `${n} в кубе равно ${n ** 3}`;
-}
-
-for (let i = 0; i <= 10; i++) {
-  console.log(cubeNumber(i));
-}
-
-/**7 задание */
-function getSeason(month) {
-  if (month < 1 || month > 12) {
-    return "Invalid month";
-  }
-
-  if (month === 12 || month <= 2) {
-    return "зима";
-  } else if (month >= 3 && month <= 5) {
-    return "весна";
-  } else if (month >= 6 && month <= 8) {
-    return "лето";
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === 10) {
+    console.log(arr[i]);
+    break;
   } else {
-    return "осень";
+    console.log(arr[i]);
+  }
+}
+//* 2 задание
+let num = [1, 5, 4, 10, 0, 3];
+
+for (let i = 0; i < num.length; i++) {
+  if (num[i] === 4) {
+    console.log("Позиция числа 4 в массиве:", i);
   }
 }
 
-console.log(getSeason(2));
-console.log(getSeason(5));
-console.log(getSeason(7));
-console.log(getSeason(10));
-console.log(getSeason(13));
+//* 3 задание
+
+let join = [1, 3, 5, 10, 20];
+
+let result = join.join(" ");
+
+console.log(result);
+
+//* 4 задание
+
+let originalArray = [];
+let evenArray = [];
+
+for (let i = 0; i < 10; i++) {
+  let randomNum = Math.floor(Math.random() * 11);
+  originalArray.push(randomNum);
+
+  if (randomNum % 2 === 0) {
+    evenArray.push(randomNum);
+  }
+}
+
+console.log("Original Array:", originalArray);
+console.log("Array with even values:", evenArray);
+
+//* 5 задание
+let multidimensionalArray = [];
+
+for (let i = 0; i < 3; i++) {
+  let innerArray = [];
+
+  for (let j = 0; j < 3; j++) {
+    innerArray.push(1);
+  }
+
+  multidimensionalArray.push(innerArray);
+}
+console.log(multidimensionalArray);
+
+//* 6 задание
+let originaliArray = [1, 1, 1];
+
+originaliArray.push(2, 2, 2);
+
+console.log(originaliArray);
+
+//* 7 задание
+let origiArray = [9, 8, 7, "a", 6, 5];
+
+origiArray.sort(function (a, b) {
+  return a - b;
+});
+
+let filteredArray = origiArray.filter(function (element) {
+  return typeof element === "number";
+});
+
+console.log(filteredArray);
+
+//* 8 задание
+let newArray = [9, 8, 7, 6, 5];
+
+let userInput = parseInt(prompt("Введите число от 1 до 10:"));
+
+if (newArray.includes(userInput)) {
+  console.log(`Номер ${userInput} находится в исходном массиве.`);
+} else {
+  console.log(`Номер ${userInput} не находится в исходном масиве`);
+}
+
+//* 9 задание
+let string = "abcdef";
+let reversedString = string.split("").reverse().join("");
+
+console.log(reversedString);
+
+//* 10 задание
+let numbers = [];
+for (let i = 0; i < 6; i++) {
+  numbers[i] = Math.floor(Math.random() * 10) + 1;
+}
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+let mean = sum / numbers.length;
+console.log(mean);
+
+//* 11 задание
+let arra = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+let newArr = [];
+for (let i = 0; i < arra.length; i++) {
+  for (let j = 0; j < arra[i].length; j++) {
+    newArr.push(arra[i][j]);
+  }
+}
+console.log(newArr);
+
+//* 12 задание
+let ary = [1, 5, 2, 8, 9, 3];
+for (let i = 0; i < ary.length - 1; i++) {
+  console.log(ary[i] + ary[i + 1]);
+}
